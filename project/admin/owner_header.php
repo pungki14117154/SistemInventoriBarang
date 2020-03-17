2fde169b987f25ddefe7eb6b4712f0543d70c4ad
edofelix <edofelix9927@gmail.com>
2020-03-18 00:31:32 +0700

    fix owner css by front-end

--- project/admin/owner_header.php
+++ project/admin/owner_header.php
@@ -4,39 +4,6 @@
 
 	<title>PAMAN TOYS (OWNER)</title>
 	<link href="owner.css" type="text/css" rel="stylesheet"/>
-	<style>
-		.header{
-			width: 100%;
-			height: 100%;
-			margin: 0.25%;
-			background-color: blue;
-			text-align: center;
-			padding-top: 10px;
-			padding-bottom: 10px;
-		}
-
-		.header a{
-			font-size: 40px;
-			font-style: bold;
-			color: white;
-		}
-
-		.left{
-			float: left;
-			margin: 0.25%;
-			width: 24%;
-			height: 100%;
-			border: 1px solid black;
-		}
-
-		.content{
-			width: 74%;
-			margin: 0.25%;
-			float: right;
-			padding: 0.25%;
-			border: 1px solid black;
-		}
-	</style>
 </head>
 
 <body>
