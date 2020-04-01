3b106e1d405fa5f796c9a3fff6bb6e854dc6ae93
edofelix <edofelix9927@gmail.com>
2020-04-01 15:28:45 +0700

    tambah css kasir dari front end dan tambah header kasir

---
+++ project/admin/kasir_header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>PAMAN TOYS (ADMIN PENJUALAN)</title>
+	<link href="kasir.css" type="text/css" rel="stylesheet"/>
+	
+</head>
+
+<body>
+
+	<div class="header">
+		<a>PAMAN TOYS</a>
+	</div>
+
+	<div class="left">
+		<ul>
+			<li><a href="kasir_pelanggan.php">Pelanggan</a></li>
+			<li><a href="kasir_stok.php">Stok</a></li>
+		</ul>
+
+	</div>
