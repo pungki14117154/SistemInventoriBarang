1768f21c9d239ec62c50496c9013d2fc99e04d5f
edofelix <edofelix9927@gmail.com>
2020-04-08 20:22:47 +0700

    fix insertion

--- project/admin/tambah_pelanggan.php
+++ project/admin/tambah_pelanggan.php
@@ -4,6 +4,14 @@
 	$nama=$_POST['nama'];
 	$tipe=$_POST['tipe'];
 
-	mysqli_query($koneksi, "insert into pelanggan values ('', '$nama', '$tipe')");
-	echo "<script>alert('Berhasil Menambahkan Pelanggan');window.location='kasir_pelanggan.php'</script>";
+	$query = mysqli_query($koneksi, "select * from pelanggan where nama_pelanggan='$nama'");
+	$r = mysqli_num_rows($query);
+
+	if ($r<1) {
+		mysqli_query($koneksi, "insert into pelanggan values ('', '$nama', '$tipe')");
+		echo "<script>alert('Berhasil Menambahkan Pelanggan');window.location='kasir_pelanggan.php'</script>";
+
+	}else {
+		echo "<script>alert('Nama Sudah Ada');window.location='kasir_pelanggan.php'</script>";
+	}
 ?>
--- project/admin/tambah_supplier.php
+++ project/admin/tambah_supplier.php
@@ -5,6 +5,14 @@
 	$almt=$_POST['alamat'];
 	$no=$_POST['no'];
 
-	mysqli_query($koneksi, "insert into supplier values ('', '$nama', '$almt', '$no')");
-	echo "<script>alert('Berhasil Menambahkan Supplier');window.location='gudang_stok.php'</script>";
+	$query = mysqli_query($koneksi, "select * from supplier where nama_supplier='$nama'");
+	$r = mysqli_num_rows($query);
+
+	if ($r<1) {
+		mysqli_query($koneksi, "insert into supplier values ('', '$nama', '$almt', '$no')");
+		echo "<script>alert('Berhasil Menambahkan Supplier');window.location='gudang_supplier.php'</script>";
+
+	}else {
+		echo "<script>alert('Nama Supplier Sudah Ada');window.location='gudang_supplier.php'</script>";
+	}
 ?>
